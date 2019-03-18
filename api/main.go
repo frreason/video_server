da4d7e233e5c05ee4cb719a3510a6def4b6540b2
frreason <13417658139@163.com>
2019-03-18 21:08:48 +0800

    again..

--- api/main.go
+++ api/main.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"net/http"
+	"video_server/api/session"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+type middleWareHandler struct {
+	r *httprouter.Router
+}
+
+func NewMiddleWareHandler(r *httprouter.Router) http.Handler {
+	m := middleWareHandler{}
+	m.r = r
+	return m
+}
+
+func (m middleWareHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//check session
+	validateUserSession(r)
+
+	m.r.ServeHTTP(w, r)
+}
 
 func RegisterHandlers() *httprouter.Router {
 	router := httprouter.New()
@@ -28,7 +46,13 @@ func RegisterHandlers() *httprouter.Router {
 	return router
 }
 
+func Prepare() {
+	session.LoadSessionsFromDB()
+}
+
 func main() {
+	Prepare()
 	r := RegisterHandlers()
-	http.ListenAndServe(":8000", r)
+	mh := NewMiddleWareHandler(r)
+	http.ListenAndServe(":8000", mh)
 }
